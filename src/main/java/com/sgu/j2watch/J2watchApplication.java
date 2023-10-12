a5b19c72d534cb32fdb661ce27509f86477a354c
grit <ngt.phongg@gmail.com>
2023-10-12 12:00:37 +0700

    upload src code

---
+++ src/main/java/com/sgu/j2watch/J2watchApplication.java
@@ -0,0 +1,13 @@
+package com.sgu.j2watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2watchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2watchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgu/j2watch/J2watchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgu.j2watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2watchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
