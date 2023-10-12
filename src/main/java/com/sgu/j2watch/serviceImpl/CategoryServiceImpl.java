77167deaf9f846970791ffdd77f6fa553217bede
itsmegrit <ngt.phongg@gmail.com>
2023-10-12 23:16:09 +0700

    u

---
+++ src/main/java/com/sgu/j2watch/serviceImpl/CategoryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.sgu.j2watch.serviceImpl;
+
+import com.sgu.j2watch.repositories.CategoryRepository;
+import com.sgu.j2watch.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CategoryServiceImpl implements CategoryService{
+
+}
--- src/main/java/com/sgu/j2watch/serviceImpl/CategoryServiceImple.java
+++
@@ -1,7 +0,0 @@
-package com.sgu.j2watch.serviceImpl;
-
-import com.sgu.j2watch.services.CategoryService;
-
-public class CategoryServiceImple implements CategoryService{
-
-}
