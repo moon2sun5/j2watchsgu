0b06f31904c465c6445a712c5542181abbb91431
itsmegrit <ngt.phongg@gmail.com>
2023-11-28 16:02:11 +0700

    update dev

--- src/main/java/com/sgu/j2watch/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/sgu/j2watch/serviceImpl/UserServiceImpl.java
@@ -52,4 +52,45 @@ public class UserServiceImpl implements UserService {
 		
 		return suitableUser;
 	}
+
+	@Override
+	public boolean updateResetPassword(String token, String email) {
+		User user = userRepository.findByEmail(email);
+
+		if(user != null) {
+			user.setResetpasswordtoken(token);
+			userRepository.save(user);
+
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	@Override
+	public User get(String reset_password_token) {
+		return userRepository.findByResetpasswordtoken(reset_password_token);
+	}
+
+	@Override
+	public void updatePassword(User user, String newPassword) {
+		Iterable<User> userList = userRepository.findAll();
+		Iterable<Account> accountList = accountRepository.findAll();
+		for(User userBaby : userList) {
+			String userToken = userBaby.getResetpasswordtoken();
+			if(userToken != null && userToken.equals(user.getResetpasswordtoken())) {
+				int id_user = userBaby.getId_user();
+				for(Account acc : accountList) {
+					if(acc.getUser_id() == id_user) {
+						user.setResetpasswordtoken(null);
+						acc.setPassword(newPassword);
+
+						userRepository.save(user);
+						accountRepository.save(acc);
+					}
+				}
+			}
+		}
+	}
 }
